66a85131a32a47d8c5cd162553c933f5e2dfab6a
aatikturk <aatikturk@gmail.com>
2022-06-05 17:14:54 +0300

    .

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 
 
-VERSION = '0.5'
+VERSION = '0.6'
 PACKAGE_NAME = 'obsstudio_sdk'
 AUTHOR = 'Adem Atikturk'
 AUTHOR_EMAIL = 'aatikturk@gmail.com'
