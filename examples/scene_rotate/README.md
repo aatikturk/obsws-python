66a06617c61035b92720ea4dd4ea3e5a61b4e5b8
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-08-31 20:13:44 +0100

    add readmes to examples.

---
+++ examples/scene_rotate/README.md
@@ -0,0 +1,14 @@
+## About
+
+Collects the names of all available scenes, rotates through them and prints their name.
+
+## Use
+
+This example assumes the existence of a `config.toml`, placed next to `__main__.py`:
+
+```toml
+[connection]
+host = "localhost"
+port = 4455
+password = "mystrongpass"
+```
--- examples/scene_rotate/__main__.py
+++ examples/scene_rotate/__main__.py
@@ -7,9 +7,9 @@ def main():
     resp = cl.get_scene_list()
     scenes = reversed(tuple(di.get("sceneName") for di in resp.scenes))
 
-    for sc in scenes:
-        print(f"Switching to scene {sc}")
-        cl.set_current_program_scene(sc)
+    for scene in scenes:
+        print(f"Switching to scene {scene}")
+        cl.set_current_program_scene(scene)
         time.sleep(0.5)
 
 
