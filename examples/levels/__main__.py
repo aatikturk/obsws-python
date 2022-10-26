9551173590decc6d43b50e66b4c8ec8e5ffa9704
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-10-26 12:13:32 +0100

    fix docstring, add docstring.

--- examples/levels/__main__.py
+++ examples/levels/__main__.py
@@ -11,12 +11,14 @@ LEVELTYPE = IntEnum(
 
 
 def on_input_mute_state_changed(data):
-    """The current program scene has changed."""
+    """An input's mute state has changed."""
     if data.input_name == DEVICE:
         print(f"{DEVICE} mute toggled")
 
 
 def on_input_volume_meters(data):
+    """volume level update every 50 milliseconds"""
+
     def fget(x):
         return round(20 * log(x, 10), 1) if x > 0 else -200.0
 
