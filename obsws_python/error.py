f3e75c0ddfe9ea600daedee5c2b7b71e038814d6
onyx-and-iris <code@onyxandiris.online>
2023-08-11 22:32:50 +0100

    OBSSDKError is now the base custom error class
    OBSSDKTimeoutError and OBSSDKRequestError subclass it
    
    req_name and error code set as error class attributes.

--- obsws_python/error.py
+++ obsws_python/error.py
@@ -1,6 +1,21 @@
 class OBSSDKError(Exception):
-    """Exception raised when general errors occur"""
+    """Base class for OBSSDK errors"""
 
 
-class OBSSDKTimeoutError(Exception):
+class OBSSDKTimeoutError(OBSSDKError):
     """Exception raised when a connection times out"""
+
+
+class OBSSDKRequestError(OBSSDKError):
+    """Exception raised when a request returns an error code"""
+
+    def __init__(self, req_name, code, message=None):
+        self.req_name = req_name
+        self.code = code
+        self.message = " ".join(
+            [
+                f"Request {self.req_name} returned code {self.code}.",
+                f"With message: {message}" if message else "",
+            ]
+        )
+        super().__init__(self.message)
